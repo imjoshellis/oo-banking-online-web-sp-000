32d781d09a00eb286e334c9df19bc6fa52ddc4bd
Josh Ellis <joshe523@gmail.com>
2020-03-04 22:34:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,18 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amt)
+    @balance += amt
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 end
