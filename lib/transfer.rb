4e4fef687313dbb0b133fe2b509e6e5531a87778
Josh Ellis <joshe523@gmail.com>
2020-03-04 22:42:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
+    if @sender.balance < @amount || !self.valid?
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif @status == "pending"
