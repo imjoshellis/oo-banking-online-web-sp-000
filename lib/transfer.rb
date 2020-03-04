7389226553c80d26409fdc0a6ecd910e312c84ed
Josh Ellis <joshe523@gmail.com>
2020-03-04 22:38:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amt)
+    @sender = sender
+    @receiver = receiver
+    @amount = amt
+    @status = pending
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
 end
